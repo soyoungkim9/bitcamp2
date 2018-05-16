cc8e528e6c9b0f10bbf0bd743f77d6bd66d7cae2
soyoungkim9 <soyoung930623@gmail.com>
2018-05-16 15:47:53 +0900

    .

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
@@ -11,6 +11,17 @@ public class Test {
         String str = "/board/add?title=aaaa&content=bbb";
         
         ServerRequest request = new ServerRequest(str);
+
+
+        
+        System.out.println(request.getServerPath());
+        System.out.println(request.getParameter("title"));
+        System.out.println(request.getParameter("content"));
+        System.out.println(request.getParameter("age"));
+        
+        System.out.println("잘부탁드립니다 ..   진호");
+
+
         System.out.println("안녕하세욘'ㅅ')/~~~~~~~ -문-");
         System.out.println(request.getServerPath());
         System.out.println("박수현 천재");
@@ -25,6 +36,10 @@ public class Test {
         System.out.println("♡♡♡♡♡♡♡♡");
         System.out.println("오현주!!!!!!!!");
         
+<<<<<<< HEAD
+        System.out.println("정호형 바보");
+
+=======
         
         System.out.println("♡♡♡♡♡♡♡♡");
         System.out.println("오현주!!!!!!!!");
@@ -34,6 +49,7 @@ public class Test {
         
         System.out.println("♡♡♡♡♡♡♡♡");
         System.out.println("김소영23323223!!!!!!!!");
+>>>>>>> branch 'master' of https://github.com/pparksuuu/bitcamp2.git
     }
 }
 
