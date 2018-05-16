fcd01c133dabc9914742a3521bb314a62e588b34
seonminmoon <seonminmoon@gmail.com>
2018-05-16 16:16:35 +0900

    .

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/member/MemberDeleteServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/member/MemberDeleteServlet.java
@@ -10,10 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import bitcamp.java106.pms.dao.BoardDao;
 import bitcamp.java106.pms.dao.MemberDao;
-import bitcamp.java106.pms.server.ServerRequest;
-import bitcamp.java106.pms.server.ServerResponse;
 import bitcamp.java106.pms.servlet.InitServlet;
 
 @SuppressWarnings("serial")
