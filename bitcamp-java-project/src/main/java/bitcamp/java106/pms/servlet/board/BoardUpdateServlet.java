a890ab0bb4093827d22458e4822367f2b508d234
pparksuuu <supr2000@gmail.com>
2018-05-16 12:55:07 +0900

    '.'

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/board/BoardUpdateServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/board/BoardUpdateServlet.java
@@ -2,7 +2,6 @@ package bitcamp.java106.pms.servlet.board;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,17 +13,14 @@ import bitcamp.java106.pms.dao.BoardDao;
 import bitcamp.java106.pms.domain.Board;
 import bitcamp.java106.pms.servlet.InitServlet;
 
-@WebServlet("/board/add")
-public class BoardAddServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+@SuppressWarnings("serial")
+@WebServlet("/board/update")
+public class BoardUpdateServlet extends HttpServlet {
     
     BoardDao boardDao;
     
     @Override
     public void init() throws ServletException {
-        // 스프링 IoC 컨테이너에서 서블릿 객체를 관리하는 것이 아니기 때문에
-        // 스프링 IoC 컨테이너에 들어 있는 DAO 객체를 자동으로 주입 받을 수 없다.
-        // 서블릿을 생성할 때 스프링 IoC 컨테이너에서 직접 DAO를 꺼내와야 한다.
         boardDao = InitServlet.getApplicationContext().getBean(BoardDao.class);
     }
     
@@ -33,14 +29,12 @@ public class BoardAddServlet extends HttpServlet {
             HttpServletRequest request, 
             HttpServletResponse response) throws ServletException, IOException {
         
-        // 클라이언트가 보낸 데이터가 어떤 문자표를 사용해서 작성한지 알아야만 
-        // String 객체(UTF-16)로 값을 꺼낼 수 있다. 
         request.setCharacterEncoding("UTF-8");
         
         Board board = new Board();
+        board.setNo(Integer.parseInt(request.getParameter("no")));
         board.setTitle(request.getParameter("title"));
         board.setContent(request.getParameter("content"));
-        board.setCreatedDate(new Date(System.currentTimeMillis()));
         
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -49,39 +43,30 @@ public class BoardAddServlet extends HttpServlet {
         out.println("<html>");
         out.println("<head>");
         out.println("<meta charset='UTF-8'>");
-        
-        // 지정된 시간이 경과하면 특정 서블릿을 요청하도록 태그를 삽입!
-        // => 웹브라우저는 meta 태그의 내용대로 동작한다.
-        //    content='경과시간(초);url=요청할URL'
-        //
         out.println("<meta http-equiv='Refresh' content='1;url=list'>");
-        
-        out.println("<title>게시물 등록</title>");
+        out.println("<title>게시물 변경</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>게시물 등록 결과</h1>");
+        out.println("<h1>게시물 변경 결과</h1>");
+        
         try {
-            boardDao.insert(board);
-            out.println("<p>등록 성공!</p>");
+            int count = boardDao.update(board);
+            if (count == 0) {
+                out.println("<p>해당 게시물이 존재하지 않습니다.</p>");
+            } else {
+                out.println("<p>변경하였습니다.</p>");
+            }
         } catch (Exception e) {
-            out.println("<p>등록 실패!</p>");
+            out.println("<p>변경 실패!</p>");
             e.printStackTrace(out);
         }
         out.println("</body>");
         out.println("</html>");
     }
-
 }
 
-//ver 37 - BoardAddController 클래스를 서블릿으로 변경
-//         출력 결과를 HTML로 변경
-//         자동 Refresh 태그 추가
+//ver 37 - BoardUpdateController를 서블릿으로 변경
+//         결과를 HTML로 출력
 //ver 31 - JDBC API가 적용된 DAO 사용
 //ver 28 - 네트워크 버전으로 변경
-//ver 26 - BoardController에서 add() 메서드를 추출하여 클래스로 정의. 
-
-
-
-
-
-
+//ver 26 - BoardController에서 update() 메서드를 추출하여 클래스로 정의.
