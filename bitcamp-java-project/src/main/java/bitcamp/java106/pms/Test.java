405ac8d2aa486a7613d21d1fa826db3c9051ec59
hongjungho <cvb1221@naver.com>
2018-05-16 14:40:35 +0900

    tt

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
@@ -15,7 +15,7 @@ public class Test {
         System.out.println(request.getParameter("title"));
         System.out.println(request.getParameter("content"));
         System.out.println(request.getParameter("age"));
-        System.out.println("^ㅡ^");
+        System.out.println("^ ㅡ ^"); 
 
     }
 
