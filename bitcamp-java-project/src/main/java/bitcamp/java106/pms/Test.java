ea3ce6c01d95fa4f79684b7f233368c908f4ebf3
hongjungho <cvb1221@naver.com>
2018-05-16 15:39:38 +0900

    ㄿ

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
@@ -16,7 +16,7 @@ public class Test {
         System.out.println(request.getParameter("title"));
         System.out.println(request.getParameter("content"));
         System.out.println(request.getParameter("age"));
+        System.out.println("화이팅");
     }
 }
 
-        
