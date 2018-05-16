625b5103c03f0a0e737d239aced646db37b35bdd
pparksuuu <supr2000@gmail.com>
2018-05-16 18:07:57 +0900

    TaskDelete,TeamAdd

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/task/TaskDeleteServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/task/TaskDeleteServlet.java
@@ -34,24 +34,36 @@ public class TaskDeleteServlet extends HttpServlet {
     protected void doGet(
             HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         response.setContentType("text/html;charset=UTF-8");
-
         PrintWriter out = response.getWriter();
+        
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+        out.println("<title>팀 삭제</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>팀 삭제 결과</h1>");
         
         try {
             int no = Integer.parseInt(request.getParameter("no"));
             int count = taskDao.delete(no);
             if (count == 0) {
-                out.println("해당 작업이 존재하지 않습니다.");
+                out.println("<p>해당 작업이 존재하지 않습니다.</p>");
             } else {
-                out.println("삭제하였습니다.");
+                out.println("<p>삭제하였습니다.</p>");
             }
         } catch (Exception e) {
-            out.println("삭제 실패!");
-            out.println("잠시 후 다시 시도해주세요. 계속 오류 발생 시 ");
-            out.println("담당자(내선: 120)에게 연락주세요.");
+            out.println("<p>삭제 실패!</p>");
+            out.println("<p>잠시 후 다시 시도해주세요. 계속 오류 발생 시 </p>");
+            out.println("<p>담당자(내선: 120)에게 연락주세요.</p>");
             e.printStackTrace(out);
         }
+        out.println("</body>");
+        out.println("</html>");
     }
 }
 
--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/team/TeamAddServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/team/TeamAddServlet.java
@@ -32,6 +32,9 @@ public class TeamAddServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        request.setCharacterEncoding("UTF-8");
+
+        
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
@@ -41,14 +44,27 @@ public class TeamAddServlet extends HttpServlet {
         team.setMaxQty(Integer.parseInt(request.getParameter("maxQty")));
         team.setStartDate(Date.valueOf(request.getParameter("startDate")));
         team.setEndDate(Date.valueOf(request.getParameter("endDate")));
-
+        
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+        
+        out.println("<title>팀 등록</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>팀 등록 결과</h1>");
+        
         try {
             teamDao.insert(team);
-            out.println("등록 성공!");
+            out.println("<p>등록 성공!</p>");
         } catch (Exception e) {
-            out.println("등록 실패!");
+            out.println("<p>등록 실패!</p>");
             e.printStackTrace(out);
         }
+        out.println("</body>");
+        out.println("</html>");
     }
 }
 
