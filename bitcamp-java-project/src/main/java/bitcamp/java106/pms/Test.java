cd1c9d2707a44a0ff58aa8e606197655dbe227aa
ohhyeonju <hyeonju3212@gmail.com>
2018-05-16 15:08:31 +0900

    현주

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
@@ -5,15 +5,6 @@ import bitcamp.java106.pms.server.ServerRequest;
 public class Test {
     
     public static void main(String[] args) {
-<<<<<<< HEAD
-        String str = "/board/add?title=aaaa&content=bbb";
-        
-        ServerRequest request = new ServerRequest(str);
-        
-        System.out.println(request.getServerPath());
-        System.out.println(request.getParameter("age"));
-        System.out.println("김소영22");
-=======
         // 프로그램을 짜다 보면 특정 API를 사용할 때가 있다.
         // 그 API를 적용하기 전에 간단한 예제를 만들어 동작을 확인하라!
         String str = "/board/add?title=aaaa&content=bbb";
@@ -24,7 +15,6 @@ public class Test {
         System.out.println(request.getParameter("title"));
         System.out.println(request.getParameter("content"));
         System.out.println(request.getParameter("age"));
->>>>>>> branch 'master' of https://github.com/pparksuuu/bitcamp2.git
         
         System.out.println("♡♡♡♡♡♡♡♡");
         System.out.println("오현주!!!!!!!!");
