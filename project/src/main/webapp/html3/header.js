d633bb33eeb3d9ebce9f7a83c8a0a4558c794407
pparksuuu <supr2000@gmail.com>
2018-06-26 18:03:46 +0900

    'project setting.'

---
+++ project/src/main/webapp/html3/header.js
@@ -0,0 +1,20 @@
+//div#header 태그에 /html/header.html 내용을 삽입한다.
+$.get("/bitcamp-java-project/html3/header.html", (data) => {
+	$("#header").html(data);
+	loadLoginUser();
+});
+
+function loadLoginUser() {	
+	// 로그인 정보를 가져와서 span#username 태그에 사용자 아이디를 삽입한다.
+	$.getJSON("/bitcamp-java-project/json/auth/loginUser", (data) => {
+		$("#username").text(data.id);
+		$("#logoutBtn").click((e) => {
+			e.preventDefault(); // 클릭했을 때 원래 하던 일이 있는데 그것을 하지 말라!
+			$.get("/bitcamp-java-project/json/auth/logout", () => {
+				location.href = "/bitcamp-java-project/html3/auth/login.html";
+			});
+		});
+	}).fail(() => {
+		location.href = "/bitcamp-java-project/html3/auth/login.html";
+	});
+}
