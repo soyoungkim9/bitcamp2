3afff0998f30135577d9e6a00e3ce53dde4e0633
seonminmoon <se0nmin@naver.com>
2018-06-27 20:44:16 +0900

    .

--- challenge-project/src/main/webapp/seonmin/header.js
+++ challenge-project/src/main/webapp/seonmin/header.js
@@ -1,5 +1,5 @@
 // div#header 태그에 /html/header.html 내용을 삽입한다.
-$.get("/challenge-project/html7/header.html", (data) => {
+$.get("/challenge-project/seonmin/header.html", (data) => {
 	$("#header").html(data);
 	loadLoginUser();
 });
@@ -11,11 +11,11 @@ function loadLoginUser() {
 		$("#logoutBtn").click((e) => {
 			e.preventDefault(); // 클릭했을 때 원래 하던 일이 있는데 그것을 하지 말라!
 			$.get("/challenge-project/json/auth/logout", () => {
-				location.href = "/project/html7/auth/login.html";
+				location.href = "/project/seonmin/auth/login.html";
 			});
 		});
 	}).fail(() => {
-		location.href = "/challenge-project/html7/auth/login.html";
+		location.href = "/challenge-project/seonmin/auth/login.html";
 	});
 	
 }
