70dc6bccaa716c6f57d3ffaab8d7a25418df89de
hongjungho <cvb1221@naver.com>
2018-07-07 07:51:43 +0900

    2018-07-07

---
+++ challenge-project/src/main/webapp/html7/jh/javascript/program.js
@@ -0,0 +1,8 @@
+
+<link rel="stylesheet" href="../../node_modules/jquery/dist/jquery.min.js">
+
+$(document).ready(function(){
+  $("#div1").fadeIn();
+  $("#div2").fadeIn("slow");
+  $("#div3").fadeIn("3000");
+});
