d20631cccefeef6284c373b074b60e5533ac8daa
pparksuuu <supr2000@gmail.com>
2018-07-13 12:27:02 +0900

    header/footer/timeline

--- challenge-project/src/main/webapp/challenge/js/header/header_black.js
+++ challenge-project/src/main/webapp/challenge/js/header/header_black.js
@@ -1,4 +1,4 @@
 //div#header 태그에 /html/header.html 내용을 삽입한다.
-$.get(serverRoot + "/html7/header/html/header_black.html", (data) => {
+$.get(serverRoot + "/challenge/html/header/header_black.html", (data) => {
 	$("#header").html(data);
 });
