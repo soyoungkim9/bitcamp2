86d5bab67e2aa0880225f0322d2c8fbc8c8bfa91
kimkwanhee <rhdwn1955@naver.com>
2018-07-19 19:39:41 +0900

    .

--- challenge-project/src/main/webapp/challenge/kh/form/member.js
+++ challenge-project/src/main/webapp/challenge/kh/form/member.js
@@ -1,18 +1,35 @@
 $("#addBtn").click(() => {
-    	$.post(serverRoot + "/json/user/add", {
-    		name: $(fName).val(),
-            sex: '남',
-            email: $(fEmail1).val() + '@' + $(fEmail2).val(),
-            /*email: $(fEmail2).val(),*/
-            password: $(fPassword1).val(),
-            /*password: $(fPassword2).val(),*/ 
-            userPhone: $(fPhone1).val() + '-' + $(fPhone2).val() + '-' + $(fPhone3).val(),
-            userPath: $(fPath).val(),
-            userType: '1'
-    	}, () => {
-    		console.log("성공했을까?");
-    	});
-    });
+	$.post(serverRoot + "/json/user/add", {
+		name: $("#fName").val(),
+		sex: $('#fGen1[value]:checked').val(),
+		sex: $('#fGen2[value]:checked').val(),
+		email: $("#fEmail1").val() + '@' + $("#fEmail2").val(),
+//		email: $(fEmail2).val(),
+		password: $("#fPassword1").val(),
+//		password: $(fPassword2).val(), 
+		userPhone: $("#fPhone1").val() + '-' + $("#fPhone2").val() + '-' + $("#fPhone3").val(),
+		userPath: $("#fPath").val(),
+		userType: '1'
+	}, () => {
+		location.href="../auth/login.html";
+	});
+});
+
+
+
+
+/*$("#addBtn").click(() => {
+	$.post(serverRoot + "/json/user/add", {
+		name: $(fName).val(),
+		email: $(fEmail1).val() + '@' + $(fEmail2).val(),
+		password: $(fPassword1).val()
+	}, () => {
+		location.href="../auth/login.html";
+	});
+});*/
+
+
+
 
 /*$("#addBtn").click(() => {
 	$.ajax({
@@ -34,23 +51,4 @@ $("#addBtn").click(() => {
 		console.error("성공???");
 	});
 });
-*/
-
-/*$("#addBtn").click(() => {
-	$.ajax({
-		type: "POST",
-		url: serverRoot + '/json/user/add',
-		data:{
-			name: $(fName).val(),
-			sex: '남',
-			email: $(fEmail).val(),
-		    password: $(fPassword).val(),
-		    userPhone: $(fPhone).val(),
-		    userPath: $(fPath).val(),
-		    userType: $(fType).val()
-			
-		}
-	}).done(function(){
-		console.log("성공했을까?");
-	});
-});*/
+ */
