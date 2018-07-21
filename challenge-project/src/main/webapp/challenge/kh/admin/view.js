320998eb2e80174ff1ff2b259f061bafdf6c6ffb
kimkwanhee <rhdwn1955@naver.com>
2018-07-21 19:57:46 +0900

    .

--- challenge-project/src/main/webapp/challenge/kh/admin/view.js
+++ challenge-project/src/main/webapp/challenge/kh/admin/view.js
@@ -4,7 +4,7 @@ var trTemplateSrc = $("#add-view").html();
 //위에서 준비한 템플릿 데이터를 가지고 HTML을 생성할 템플릿 엔진 준비
 var templateFn = Handlebars.compile(trTemplateSrc);
 
-$.getJSON(serverRoot + "/json/admin/list", (data) => {
+$.getJSON(serverRoot + "/json/challenge/list", (data) => {
 	//$tableBody.innerHTML = templateFn({list:data});
     $("#ccc").html(templateFn({list:data}));
 });
