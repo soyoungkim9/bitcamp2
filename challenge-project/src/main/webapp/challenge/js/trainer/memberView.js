9aca63289d27d12f83f9cd93681b4d67452d456f
soyoungkim9 <soyoung930623@gmail.com>
2018-07-22 18:40:04 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/trainer/memberView.js
+++ challenge-project/src/main/webapp/challenge/js/trainer/memberView.js
@@ -1,18 +1,21 @@
-if (location.href.split("?").length > 1) {    
-	var no = location.href.split("?")[1].split("=")[1];
-	
-	$.getJSON(serverRoot + "/json/programMember/" + no, function(data) {
-        $("#mName").append(data.members.name);
-        $("#mSex").append(data.members.sex);
-        $("#mTel").append(data.members.userPhone);
-        $("#mProgram").append(data.name);
-        $("#mTarget").append(data.proGoal);
-        $("#mAttend").append(data.diary.dcheck);
-        $("#mTargetPer").append(data.proGoalNum);
+if (location.href.split("?").length > 1) {
+	var pno = location.href.split("?")[1].split("&")[0].split("=")[1];
+	var uno = location.href.split("&")[1].split("=")[1];
+
+	$.getJSON(serverRoot + "/json/programMember/" + pno + "/" + uno, function(data) {
+		$("#mName").append(data[0].users.name);
+        $("#mSex").append(data[0].users.sex);
+        $("#mTel").append(data[0].users.userPhone);
+        $("#mProgram").append(data[0].name);
+				$("#mDate1").append(data[0].startDate);
+				$("#mDate2").append(data[0].endDate);
+        $("#mTarget").append(data[0].proGoal);
+        $("#mAttend").append(data[0].diarys.dcheck);
+        $("#mTargetPer").append(data[0].proGoalNum);
 	});
-	
+
 	$(".close").click(function() {
 		location.href= serverRoot + "/challenge/html/trainer/memberList.html";
 		});
-	
+
 }
