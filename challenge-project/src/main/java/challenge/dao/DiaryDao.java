0869c5f62f2c2fc6ba8b680ae0417af49612fc1c
kimkwanhee <rhdwn1955@naver.com>
2018-07-24 12:18:37 +0900

    .

--- challenge-project/src/main/java/challenge/dao/DiaryDao.java
+++ challenge-project/src/main/java/challenge/dao/DiaryDao.java
@@ -2,13 +2,13 @@ package challenge.dao;
 
 import java.util.List;
 
-import challenge.domain.BodyInfo;
+import challenge.domain.Diary;
 
 public interface DiaryDao {
     int delete(int no);
-    List<BodyInfo> selectList();
-    int insert(BodyInfo diary);
-    int update(BodyInfo diary);
-    BodyInfo selectOne(int no);
+    List<Diary> selectList();
+    int insert(Diary diary);
+    int update(Diary diary);
+    Diary selectOne(int no);
 
 }
