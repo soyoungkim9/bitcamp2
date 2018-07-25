d1669a141364e94389a0f970d7cce8461b3bab46
ohhyeonju <hyeonju3212@gmail.com>
2018-07-25 14:11:20 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/trainer/file-upload.js
+++ challenge-project/src/main/webapp/challenge/js/trainer/file-upload.js
@@ -1,3 +1,7 @@
+/*// 상세미디어
+var imgFiles;
+var subImglen;
+var subImg;
 $('#fileupload2').fileupload({
   url: '../../../json/fileupload2/upload02',        // 서버에 요청할 URL
   dataType: 'json',         // 서버가 보낸 응답이 JSON임을 지정하기
@@ -6,40 +10,62 @@ $('#fileupload2').fileupload({
   autoUpload: false,        // 파일을 추가할 때 자동 업로딩 하지 않도록 설정.
   disableImageResize: /Android(?!.*Chrome)|Opera/
         .test(window.navigator && navigator.userAgent), // 안드로이드와 오페라 브라우저는 크기 조정 비활성 시키기
+
+  imageMaxWidth: 800,
+  imageMaxHeight: 800,
+  imageCrop: true, // Force cropped images
+        
   previewMaxWidth: 100,   // 미리보기 이미지 너비
   previewMaxHeight: 100,  // 미리보기 이미지 높이 
   previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
   processalways: function(e, data) {
       console.log('fileuploadprocessalways()...');
       console.log(data.files);
+      imgFiles = data.files;
       var imagesDiv = $('#ad-images-div');
       imagesDiv.html("");
       for (var i = 0; i < data.files.length; i++) {
         try {
           if (data.files[i].preview.toDataURL) {
-            $("<img>").attr('src', data.files[i].preview.toDataURL()).css('width', '100px').appendTo(imagesDiv);
+            var imgWrapper = $('<div>')
+            .attr("data-file-index", i)
+            .addClass('ad-adImgs-wrapper')
+            .click(delImg)
+            .appendTo(imagesDiv);
+    var imgContent = $('<div>')
+            .addClass('ad-adImgs-content')
+            .appendTo(imgWrapper);
+    var imgCover = $('<div>')
+            .addClass('ad-adImgs-cover')
+            .appendTo(imgWrapper);
+    $("<img/>").attr('src', data.files[i].preview.toDataURL()).appendTo(imgContent).addClass('ad-adImgs');
           }
         } catch (err) {}
       }
-      $('#addBtn').unbind("click");
+      //$('#addBtn').unbind("click");
       $('#addBtn').click(function() {
           data.submit();
       });
   }, 
   submit: function (e, data) { // 서버에 전송하기 직전에 호출된다.
-    console.log('submit()...');
-    data.formData = {
-        name: $('#name').val(),
-        age: $('#age').val()
-    };
+    console.log('submit2()...');
   }, 
   done: function (e, data) { // 서버에서 응답이 오면 호출된다. 각 파일 별로 호출된다.
-    console.log('done()...');
+    console.log('done2()...');
     console.log(data.result);
-    $('<p/>').text("name : " + data.result.name).appendTo(document.body);
-    $('<p/>').text("age : " + data.result.age).appendTo(document.body);
+    subImglen = data.result.files.length
+    subImg = data.result.files
+    console.log(data.result.files[0].filename)
+    
+    
     $.each(data.result.files, function(index, file) {
       $('<p/>').text(file.filename + " : " + file.filesize).appendTo(document.body);
     });
   }
 });
+function delImg(event){
+  var wrapperDiv = $(event.currentTarget);
+  wrapperDiv.remove();
+    var fileIndex =wrapperDiv.attr('data-file-index');
+    imgFiles.splice(fileIndex, 1);
+}*/
--- challenge-project/src/main/webapp/challenge/js/trainer/program-upload.js
+++ challenge-project/src/main/webapp/challenge/js/trainer/program-upload.js
@@ -1,3 +1,4 @@
+// 챌린지 가져오기
 var chalBody = $("#proInsert").html();
 
 var chalBodyFn = Handlebars.compile(chalBody);
@@ -66,14 +67,95 @@ function sample6_execDaumPostcode() {
 }
 
 
+//상세미디어
+var imgFiles;
+var subImglen;
+var subImg;
+$('#fileupload2').fileupload({
+  url: '../../../json/fileupload2/upload02',        // 서버에 요청할 URL
+  dataType: 'json',         // 서버가 보낸 응답이 JSON임을 지정하기
+  sequentialUploads: true,  // 여러 개의 파일을 업로드 할 때 순서대로 요청하기.
+  singleFileUploads: false, // 한 요청에 여러 개의 파일을 전송시키기.
+  autoUpload: false,        // 파일을 추가할 때 자동 업로딩 하지 않도록 설정.
+  disableImageResize: /Android(?!.*Chrome)|Opera/
+        .test(window.navigator && navigator.userAgent), // 안드로이드와 오페라 브라우저는 크기 조정 비활성 시키기
+
+  imageMaxWidth: 800,
+  imageMaxHeight: 800,
+  imageCrop: true, // Force cropped images
+        
+  previewMaxWidth: 100,   // 미리보기 이미지 너비
+  previewMaxHeight: 100,  // 미리보기 이미지 높이 
+  previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
+  processalways: function(e, data) {
+      console.log('fileuploadprocessalways()...');
+      console.log(data.files);
+      imgFiles = data.files;
+      var imagesDiv = $('#ad-images-div');
+      imagesDiv.html("");
+      for (var i = 0; i < data.files.length; i++) {
+        try {
+          if (data.files[i].preview.toDataURL) {
+            var imgWrapper = $('<div>')
+            .attr("data-file-index", i)
+            .addClass('ad-adImgs-wrapper')
+            .click(delImg)
+            .appendTo(imagesDiv);
+    var imgContent = $('<div>')
+            .addClass('ad-adImgs-content')
+            .appendTo(imgWrapper);
+    var imgCover = $('<div>')
+            .addClass('ad-adImgs-cover')
+            .appendTo(imgWrapper);
+    $("<img/>").attr('src', data.files[i].preview.toDataURL()).appendTo(imgContent).addClass('ad-adImgs');
+          }
+        } catch (err) {}
+      }
+      //$('#addBtn').unbind("click");
+      $('#addBtn').click(function() {
+          data.submit();
+      });
+  }, 
+  submit: function (e, data) { // 서버에 전송하기 직전에 호출된다.
+    console.log('submit2()...');
+  }, 
+  done: function (e, data) { // 서버에서 응답이 오면 호출된다. 각 파일 별로 호출된다.
+    console.log('done2()...');
+    console.log(data.result);
+    subImglen = data.result.files.length
+    subImg = data.result.files
+    console.log(data.result.files[0].filename)
+    
+    
+    /*$.each(data.result.files, function(index, file) {
+      $('<p/>').text(file.filename + " : " + file.filesize).appendTo(document.body);
+    });*/
+  }
+});
+function delImg(event){
+  var wrapperDiv = $(event.currentTarget);
+  wrapperDiv.remove();
+    var fileIndex =wrapperDiv.attr('data-file-index');
+    imgFiles.splice(fileIndex, 1);
+}
+
+
+
+// 메인 이미지 업로드
 $('#fileupload').fileupload({
     url: '../../../json/fileupload2/upload01',        // 서버에 요청할 URL
+    async: false,
     dataType: 'json',         // 서버가 보낸 응답이 JSON임을 지정하기
     sequentialUploads: true,  // 여러 개의 파일을 업로드 할 때 순서대로 요청하기.
     singleFileUploads: false, // 한 요청에 여러 개의 파일을 전송시키기.
     autoUpload: false,        // 파일을 추가할 때 자동 업로딩 하지 않도록 설정.
     disableImageResize: /Android(?!.*Chrome)|Opera/
           .test(window.navigator && navigator.userAgent), // 안드로이드와 오페라 브라우저는 크기 조정 비활성 시키기
+    
+    imageMaxWidth: 800,
+    imageMaxHeight: 800,
+    imageCrop: true, // Force cropped images
+    
     previewMaxWidth: 250,   // 미리보기 이미지 너비
     previewMaxHeight: 250,  // 미리보기 이미지 높이 
     previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
@@ -85,11 +167,15 @@ $('#fileupload').fileupload({
         for (var i = 0; i < data.files.length; i++) {
           try {
             if (data.files[i].preview.toDataURL) {
-              $("<img>").attr('src', data.files[i].preview.toDataURL()).css('width', '250px').appendTo(imagesDiv);
+              $(".filebox label").css('background-image', 
+                                      'url(' + data.files[i].preview.toDataURL() + ')')
+              /*$("<img>").attr('src', data.files[i].preview.toDataURL())
+                        .css('width', '250px')
+                        .appendTo(imagesDiv)*/
             }
           } catch (err) {}
         }
-        $('#addBtn').unbind("click");
+        //$('#addBtn').unbind("click");
         $('#addBtn').click(function() {
             data.submit();
         });
@@ -102,30 +188,31 @@ $('#fileupload').fileupload({
       console.log(data.result)
       var mainImg = data.result.filename;
       
-      var ttt = ['','','','','','',''];
+      
+      // 요일, 시간
+      var selectTime = ['','','','','','',''];
       $(".time-slot[data-selected]").each(function(i, tag) {
         var e = $(tag);
-        for(i = 0; i < ttt.length; i++) {
+        for(i = 0; i < selectTime.length; i++) {
           if (e.attr('data-day') == i) {
-            ttt[i] += e.attr('data-time') + ',';
+            selectTime[i] += e.attr('data-time') + ',';
           }
         }
       });
-      console.log(ttt);
-      
       var proDay = '';
       var proTime = '';
-      for (i = 0; i < ttt.length; i++) {
-        if (ttt[i] !== '') {
+      for (i = 0; i < selectTime.length; i++) {
+        if (selectTime[i] !== '') {
           proDay += i + ' ';
-          proTime += ttt[i] + ' ';
+          proTime += selectTime[i] + ' ';
         }
       }
-      console.log(proDay)
-      console.log(proTime)
       
+      
+      // 프로그램 DB 입력
       $.ajax({
         type: 'POST',
+        async: false,
         url: serverRoot + '/json/program/add',
         data: {
           postNo: $(sample6_postcode).val(),
@@ -150,30 +237,45 @@ $('#fileupload').fileupload({
           mainImg: mainImg
         }, 
       }).done(function() {
-        console.log('입력됨.11');
-        location.href = 'trainerPage-programList.html';
+        console.log('상세미디어등록전')
+        
+        // 프로그램 미디어
+        for (var i = 0; i < subImglen; i++) {
+          console.log('상세미디어 for문')
+          $.ajax({
+            type: 'POST',
+            url: serverRoot + '/json/programMedia/add',
+            data: {
+              path: subImg[i].filename,
+              pmtype: 1
+            }, 
+          }).done(function() {
+            console.log('머지?')
+          });
+        }
+        /*location.href = 'trainerPage-programList.html';*/
       });
     }
 });
 
 /*$("#addBtn").click(() => {
-  var ttt = ['','','','','','',''];
+  var selectTime = ['','','','','','',''];
   $(".time-slot[data-selected]").each(function(i, tag) {
     var e = $(tag);
-    for(i = 0; i < ttt.length; i++) {
+    for(i = 0; i < selectTime.length; i++) {
       if (e.attr('data-day') == i) {
-        ttt[i] += e.attr('data-time') + ',';
+        selectTime[i] += e.attr('data-time') + ',';
       }
     }
   });
-  console.log(ttt);
+  console.log(selectTime);
   
   var proDay = '';
   var proTime = '';
-  for (i = 0; i < ttt.length; i++) {
-    if (ttt[i] !== '') {
+  for (i = 0; i < selectTime.length; i++) {
+    if (selectTime[i] !== '') {
       proDay += i + ' ';
-      proTime += ttt[i] + ' ';
+      proTime += selectTime[i] + ' ';
     }
   }
   console.log(proDay)
