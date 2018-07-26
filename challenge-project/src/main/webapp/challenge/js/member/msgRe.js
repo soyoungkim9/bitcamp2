bba6dd6c19767c15794e2f5fb8f640e43742359c
seonminmoon <se0nmin@naver.com>
2018-07-26 18:42:36 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/member/msgRe.js
+++ challenge-project/src/main/webapp/challenge/js/member/msgRe.js
@@ -4,9 +4,6 @@ $(".close").click(function() {
 $("#msg-ok").click(function() {
 	location.href= serverRoot + "/challenge/html/member/member-msg.html";
 });
-$("#msg-re").click(function() {
-	location.href= serverRoot + "/challenge/html/member/member-msgRe.html";
-});
 
 if (location.href.split("?").length > 1) {
 	// var msgno = location.href.split("?")[1].split("&")[0].split("=")[1];
@@ -14,9 +11,8 @@ if (location.href.split("?").length > 1) {
 	
 
 	$.getJSON(serverRoot + "/json/message/" + msgno, function(data) {
-		// console.log(data);
-		$("#mName").append(data.trainer.name);
-        $("#mDate").append(data.msgDate);
+		console.log(data);
+		$("#mName").append(data.member.name);
         $("#mTitl").append(data.title);
         $("#mCont").append(data.content);
 	});
