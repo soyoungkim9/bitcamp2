3064ba5b972b4329ba4a232914ec7d27446312c5
kimkwanhee <rhdwn1955@naver.com>
2018-07-26 19:32:03 +0900

    .

--- challenge-project/src/main/webapp/challenge/html/member/member-history4.js
+++ challenge-project/src/main/webapp/challenge/html/member/member-history4.js
@@ -1,18 +1,10 @@
-var trTemplateSrc = $("#tr-template2").html();
+var trTemplateSrc = $("#tr-template").html();
 
 //위에서 준비한 템플릿 데이터를 가지고 HTML을 생성할 템플릿 엔진 준비
 var templateFn = Handlebars.compile(trTemplateSrc);
-/*var data = {
-        email : $("#fId").val(),
-        password: $("#fPassword").val()
-    };
-	
+
+$.getJSON(serverRoot + "/json/user/list", (data) => {
 	console.log(data);
-	
-    if ($(document.body).is("#fSaveId:checked")) {
-        data.saveId = "true";
-    }*/
-$.getJSON(serverRoot + "/json/program/list", (data) => {
 	//$tableBody.innerHTML = templateFn({list:data});
-    $("#listbody2").html(templateFn({list:data}));
+  $("#story").html(templateFn({list:data}));
 });
