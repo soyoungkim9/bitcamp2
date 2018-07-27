c8f642cea69279eedb38fa6d3dcea948f23fc1c5
kimkwanhee <rhdwn1955@naver.com>
2018-07-27 18:18:07 +0900

    .

--- challenge-project/src/main/webapp/challenge/html/member/member-history4.js
+++ challenge-project/src/main/webapp/challenge/html/member/member-history4.js
@@ -3,8 +3,28 @@ var trTemplateSrc = $("#tr-template").html();
 //위에서 준비한 템플릿 데이터를 가지고 HTML을 생성할 템플릿 엔진 준비
 var templateFn = Handlebars.compile(trTemplateSrc);
 
-$.getJSON(serverRoot + "/json/user/list", (data) => {
-	console.log(data);
+$.getJSON(serverRoot + "/json/program/list", (data) => {
+	/*console.log(data);*/
 	//$tableBody.innerHTML = templateFn({list:data});
   $("#story").html(templateFn({list:data}));
+});
+
+$(function () {
+    $("#toryBtn").ready(function (e) {
+      
+            $.ajax({
+                type: 'GET',
+                url: serverRoot + '/json/program/list',
+                dataType:'json',
+                async: false,
+                success: function (data) {
+                	$(abcd).append(data.startDate);
+                	console.log(data);
+
+                },
+                error: function (response) {
+                	console.log("civa");
+                }
+            });
+    });
 });
