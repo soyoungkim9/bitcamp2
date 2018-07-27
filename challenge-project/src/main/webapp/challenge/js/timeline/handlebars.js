e084da2bdcfcb01d67be1080a5befd3f7b5e415c
pparksuuu <supr2000@gmail.com>
2018-07-27 11:08:11 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/timeline/handlebars.js
+++ challenge-project/src/main/webapp/challenge/js/timeline/handlebars.js
@@ -2,12 +2,14 @@
 $(document).ready(function() {
 	// 글쓰기 모달에 글쓴이 이름 출력
 	$(".tl-user-name-json").html(obj.name);
-
+	// 스크롤 위치 맨 위로 고정
+	
 	var pageCount = 1; // 타임라인 첫번째 page 카운트임.
 	loadCards(pageCount);
 
 	$(window).on('scroll',function () {
 		infiniteScroll();
+//		console.log("documentHeight : " + $(document).height() + ", scrollHeight" + $(window).scrollTop() + $(window).height())	
 	});
 
 	function infiniteScroll() {
@@ -16,10 +18,10 @@ $(document).ready(function() {
 
 		// 스크롤한 높이와 문서의 높이가 같을 때
 		if (documentHeight <= scrollHeight + 100) { // 스크롤이 바닥에 닿으면?
-			setTimeout(loadCards(pageCount), 1000);
+			setTimeout(loadCards(pageCount), 3000);
 			pageCount++;
 			console.log("무한스크롤 다음 카드 불러옴! pageCount 값 : " + pageCount);
-
+//			console.log("documentHeight : " + documentHeight + ", scrollHeight" + scrollHeight)			
 		}
 	}
 
@@ -59,7 +61,6 @@ function loadComments(cardNo) {
 
 // 좋아요 체크 여부
 function timelineLikeChecked(cardNo) {
-	console.log(cardNo)
 	$.post({
 		url: "../../../json/timeline/isChecked",
 		data: {
@@ -69,13 +70,8 @@ function timelineLikeChecked(cardNo) {
 		},
 		async: false
 	}).done(function(isChecked) {
-		$('.lk' + cardNo).prepend("회원님 외 ");
 		if (isChecked) {
-			console.log("좋아요 체크 여부" + cardNo);
-			console.log($('.lk' + cardNo))
-					
-			$('.lk' + cardNo).html("회원님 외 ");
-			$('.lk-thumbs' + cardNo).attr("color", "blue")
+			$('.lk-thumbs' + cardNo).attr("style", "color:#DD1F26;")
 			$('.lk' + cardNo).prepend("회원님 외 ");
 		}
 	});
@@ -83,10 +79,8 @@ function timelineLikeChecked(cardNo) {
 
 // 좋아요 개수 카운트
 function timelineLikeCount (cardNo) {
-	$.get(serverRoot + "/json/timeline/timelineLikeCount/" + cardNo, (data) => {
-			$('.lk' + cardNo).html(data);
-	})
-	
-	console.log("좋아요 개수 카운트 함수" + cardNo);
-	timelineLikeChecked(cardNo);
+	$.get(serverRoot + "/json/timeline/timelineLikeCount/" + cardNo, function(data) {
+		$('.lk' + cardNo).html(data);
+		timelineLikeChecked(cardNo)
+	});
 }
--- challenge-project/src/main/webapp/challenge/js/timeline/timeline.js
+++ challenge-project/src/main/webapp/challenge/js/timeline/timeline.js
@@ -73,42 +73,6 @@ function resizeFunction() {
 }
 
 
-
-/* 무한 스크롤 */
-//무한 스크롤 부분
-/*
-$(document).ready(function() {
-  // 스크롤 이벤트 발생시
-	
-  $(window).scroll(function() {
-    // 필요한 변수 구하기
-	  var documentHeight = $(document).height();
-    var scrollHeight = $(window).scrollTop() + $(window).height();
-    		console.log("scrollHeight : " + scrollHeight);
-    		console.log("documentHeight : " + documentHeight);
-
-
-
-    // 스크롤한 높이와 문서의 높이가 같을 때
-    if (documentHeight <= scrollHeight + 100) {
-    	/*
-    	$.ajax({
-    		type:'GET',
-    		url: serverRoot + 'json/timeline/list'
-    	});
-
-    	 */
-      /*
-      for (var i = 0; i < 5; i++) {
-        $(' <div class="sh-tl-card sh-card" style="border: 2px solid blue;"><section class="sh-tl-card-primary"><div class="sh-tl-user"><i class="sh-tl-user-circle fas fa-user-circle"></i><h1 class="sh-tl-user-name">무한스크롤</h1></div></section><div class="sh-tl-card-content"><p>졸려죽겠네? 아니야 할 수 있다! 북극곰 짱</p></div><section class="sh-tl-card-actions"><div class="sh-tl-like-count"><a href="#!"><i class="far fa-thumbs-up"></i></a><a id="sh-tl-CountedClicks" href="#!">0 명이 좋아합니다.</a><a href="#!"></a></div><div class="sh-tl-card-bottom"><div class="sh-tl-like sh-tl-card-bottom-items" style="border: 1px solid black;"><a onclick="TlAddClick()" href="#!"><i class="far fa-thumbs-up"></i>좋아요</a></div><div class="sh-tl-comment sh-tl-card-bottom-items" style="border: 1px solid black;"><a href="#!"><i class="far fa-comments"></i>댓글달기</a></div></div></section><section class="sh-tl-card-reply"><div class="sh-tl-reply-user"><i class="sh-tl-reply-user-circle fas fa-user-circle"></i><h1 class="sh-tl-reply-write">댓글을 입력하세요</h1></div></section><div style="clear:both;"></div></div>').appendTo('.sh-infinite-scroll');
-      }
-    }
-    
-  });
-  
-});
-       */
-
 /* modal event */
 // Get the modal
 var modal = document.getElementById('sh-tl-myModal');
