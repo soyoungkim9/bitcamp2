084f41e8b7c9ed14b1f5067af219f58aab8eeabd
seonminmoon <se0nmin@naver.com>
2018-07-30 20:07:37 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/member/msgRe.js
+++ challenge-project/src/main/webapp/challenge/js/member/msgRe.js
@@ -14,9 +14,17 @@ $("#addBtn").click(() => {
 	}).done(function(){
 		location.href="member-msg.html"
 	});
-	console.log("Hey")
 });
-
+/*
+ $.post({
+  url: add
+  data : {
+   보내는시람 : obj.uno(?),
+   메세지방향 : 1 또는 2,
+   받는 사람 : 네가 끌어온 정보
+   그 외 제목,내용
+}
+ */
 
 if (location.href.split("?").length > 1) {
 	// var msgno = location.href.split("?")[1].split("&")[0].split("=")[1];
