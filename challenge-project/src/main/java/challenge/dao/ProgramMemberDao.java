53ca26e5d7fc01053d556518bf5f731f3c3ad422
hongjungho <cvb1221@naver.com>
2018-07-31 14:38:39 +0900

    11

--- challenge-project/src/main/java/challenge/dao/ProgramMemberDao.java
+++ challenge-project/src/main/java/challenge/dao/ProgramMemberDao.java
@@ -2,6 +2,7 @@ package challenge.dao;
 
 import java.util.List;
 
+import challenge.domain.Member;
 import challenge.domain.ProgramMember;
 
 // 트레이너 - 회원관리
@@ -10,7 +11,7 @@ public interface ProgramMemberDao {
     List<ProgramMember> selectList(int trnNo); // 전체 리스트
     List<ProgramMember> selectListWithProgram(int pno, int trnNo); // 프로그램에 속한 회원 목록 뽑기
     List<ProgramMember> selectOne(int pno, int userNo); // 한명만 선택해서 보기
-    List<ProgramMember> selectListWithUno(int userNo); // 유저가 듣는 프로그램 모든 정보 받아오기
+    Member selectListWithUno(int userNo); // 유저가 듣는 프로그램 모든 정보 받아오기
   
 }
 
--- challenge-project/src/main/java/challenge/service/ProgramMemberService.java
+++ challenge-project/src/main/java/challenge/service/ProgramMemberService.java
@@ -2,6 +2,7 @@ package challenge.service;
 
 import java.util.List;
 
+import challenge.domain.Member;
 import challenge.domain.ProgramMember;
 
 // 트레이너 - 회원관리
@@ -10,6 +11,6 @@ public interface ProgramMemberService {
     List<ProgramMember> list(int trnNo); // 전체 프로그램에 참여중인 회원 목록
     List<ProgramMember> list(int pno, int trnNo); // 해당 프로그램에 참여중인 회원 목록
     List<ProgramMember> get(int pno, int userNo); // 한 회원의 정보 상세보기
-    List<ProgramMember> getWithUserNo(int userNo);
+    Member getWithUserNo(int userNo);
 
 }
