b489f953a6450a4742486733400b5749c19ac191
soyoungkim9 <soyoung930623@gmail.com>
2018-08-01 19:13:38 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/member/trainerReportList.js
+++ challenge-project/src/main/webapp/challenge/js/member/trainerReportList.js
@@ -8,19 +8,18 @@ var endDate;
 var liTemplateSrc = $("#li-template").html();
 var templateFn = Handlebars.compile(liTemplateSrc);
 $(document).ready(function() {
-   $.ajax(serverRoot + "/json/plan/pList/" + userInfo.userNo, {
+   $.ajax(serverRoot + "/json/programMember/lList/" + userInfo.userNo, {
       dataType: "json",
        success(data) {
          $('#programList').html(templateFn({list:data}));
-         defaultPage = $('.active').find('a').attr('data-last');
+         defaultPage = $('.active').find('a').attr('data-no');
          startDate = $('.active').find('a').attr('data-sdt');
          endDate = $('.active').find('a').attr('data-edt');
          // 운동일지 default page 설정
          $.ajax(serverRoot + "/json/plan/list/" + defaultPage, {
             dataType: "json",   
              success(data) {
-                console.log(obj.userNo);
-                if(data.length == 0) {
+            	if(data.length == 0) {
                    $('#programBox').append('<h4>기간: <span id="sdt">' +
                          startDate + '</span> ~ <span id="edt">' +
                          endDate + '</span></h4>');
@@ -43,6 +42,7 @@ $(document).ready(function() {
        }
    });
 });
+
 
 // 운동일지 리스트 보기
 var programTemplateSrc = $("#program-template").html();
@@ -103,44 +103,10 @@ window.onclick = function(event) {
     }
 }
 
-// 새글 관련 이벤트
-$("#addPlan").click(() => {
-    console.log(startDate, endDate);
-   pno = $('.active').find('a').attr('data-no');
-    // 프로그램 회차 관련
-    $.ajax(serverRoot + "/json/plan/list/" + pno, {
-       dataType: "json",
-       success(data) {
-          $("option").remove();
-          $("#mDate").val('');
-          $("#mTitle").val('');
-          $("#mContent").val('');
-          var count = data.length + 1;
-          var turn;
-          if(data.length == 0) { // 프로그램에 운동일지 하나도 없을 경우
-             turn = 1;
-          } else { // 프로그램에 운동일지 한 개 이상 있을 경우
-             turn = data[0].program.proTurn;
-          }
-          for (count; count <= turn; count++) {
-             $('#mTurn').append("<option>" + count +"</option>");
-          }
-       },
-       error() {
-           window.alert("report.js addPlan 실행 오류!");
-       }
-    });
-    
-    $('.modal').css("display", "block");
-    $('#viewForm').css("display", "none");
-    $('#addForm').css("display", "block");
-});
-
 //원하는 회차의 운동일지 확대 해서 보기!
 $(document.body).on('click', '.editIcon', function(event) {
    event.preventDefault();
    plno = $(this).find('a').attr('data-no');
-   pno = $(this).find('a').attr('data-pno');
    
    $('.modal').css("display", "block");
    $('#addForm').css("display", "none");
@@ -162,56 +128,4 @@ $(document.body).on('click', '.editIcon', function(event) {
    });
 });
 
-// 수정 버튼 눌렀을 경우 쿼리
-$("#updatePlanButton").click(() => {
-   $.post(serverRoot + "/json/plan/update/", {
-      planTitl: $("#modalViewTitle input").val(),
-      planContent: $("#modalViewContent textarea").val(),
-      "no": plno
-   }, () => {
-      modal.style.display = "none";
-      
-      /* 업데이트한 상태에서 이전 화면으로 돌아가기 위한 코드임... 뭔가 이상 */
-      $.ajax(serverRoot + "/json/plan/list/" + pno, {
-         dataType: "json",   
-          success(data) {
-             $('#programBox').html(programTemplateFn({
-                name: data[0].program.name,
-                startDate: data[0].program.startDate,
-                endDate: data[0].program.endDate,
-                list:data}));
-          },
-          error() {
-              window.alert("report.js view list 실행 오류!");
-          }   
-      });
-      
-   });
-});
 
-// 등록 버튼 눌렀을 시
-$("#registerPlan").click(() => {   
-   $.post(serverRoot + "/json/plan/add", {
-      planTurn: $("#mTurn").val(),
-      planDate: $("#mDate").val(),
-      planTitl: $("#mTitle").val(),
-      planContent: $("#mContent").val(),
-      "program.no": pno
-   }, () => {
-      modal.style.display = "none";
-      /* 업데이트한 상태에서 이전 화면으로 돌아가기 위한 코드임... 뭔가 이상 */
-      $.ajax(serverRoot + "/json/plan/list/" + pno, {
-         dataType: "json",   
-          success(data) {
-             $('#programBox').html(programTemplateFn({
-                name: data[0].program.name,
-                startDate: data[0].program.startDate,
-                endDate: data[0].program.endDate,
-                list:data}));
-          },
-          error() {
-              window.alert("report.js view list 실행 오류!");
-          }   
-      });
-   });
-});
