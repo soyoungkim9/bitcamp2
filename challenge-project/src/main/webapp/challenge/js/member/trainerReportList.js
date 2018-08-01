006d3508bfe59c25d501d5ba189f064f5d026823
soyoungkim9 <soyoung930623@gmail.com>
2018-08-01 17:06:29 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/member/trainerReportList.js
+++ challenge-project/src/main/webapp/challenge/js/member/trainerReportList.js
@@ -8,7 +8,7 @@ var endDate;
 var liTemplateSrc = $("#li-template").html();
 var templateFn = Handlebars.compile(liTemplateSrc);
 $(document).ready(function() {
-	$.ajax(serverRoot + "/json/plan/pList/" + obj.userNo, {
+	$.ajax(serverRoot + "/json/plan/pList/" + userInfo.userNo, {
 		dataType: "json",
 	    success(data) {
 			$('#programList').html(templateFn({list:data}));
@@ -214,18 +214,5 @@ $("#registerPlan").click(() => {
 		    }	
 		});
 	});
-});
-
-// DatePicker
-$(function() {
-    $("#mDate").datepicker({
-        dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
-        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-        monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'],
-        monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
-        minDate: startDate,
-        maxDate: endDate,
-        dateFormat: "yy-mm-dd"
-    });
 });
 
