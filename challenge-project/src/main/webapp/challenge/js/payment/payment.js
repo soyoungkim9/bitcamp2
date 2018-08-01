36a206a90a06025c8f745401a2429daab16e2f8c
hongjungho <cvb1221@naver.com>
2018-08-01 10:22:08 +0900

    1

--- challenge-project/src/main/webapp/challenge/js/payment/payment.js
+++ challenge-project/src/main/webapp/challenge/js/payment/payment.js
@@ -1,20 +1,20 @@
 
 
 if (location.href.split("?").length > 1) {
-		
+
 	var no = location.href.split("?")[1].split("=")[1];
-	
+
 	$.getJSON(serverRoot + "/json/program/" + no, (data) => {
 
-	
+
 		$('.project-title').append("<b>"+data.name+"</b>");
 		function addComma(num) {
 			var regexp = /\B(?=(\d{3})+(?!\d))/g;
 			return num.toString().replace(regexp, ',');
 		}
 		var num = data.price;
-		
-		
+
+
 
 		$('.p-price').append(addComma(num)+" 원");
 		$('.p-trainer').append(data.trainerNo.name);
@@ -65,71 +65,77 @@ if (location.href.split("?").length > 1) {
 	}
 
 
-//--------------------------카카오페이 api--------------------------//
+//	--------------------------카카오페이 api--------------------------//
 
 
-function requestPay() {
+	function requestPay() {
 
-	$.getJSON(serverRoot + "/json/program/" + no, (data) => {
+		$.getJSON(serverRoot + "/json/program/" + no, (data) => {
 
-		IMP.init("imp63287981"); // "imp00000000" 대신 발급받은 "가맹점 식별코드"를 사용합니다.
-		IMP.request_pay({
+			IMP.init("imp63287981"); // "imp00000000" 대신 발급받은 "가맹점 식별코드"를 사용합니다.
+			IMP.request_pay({
 
-			pg : 'kakaopay',
-			pay_method : 'card',
-			merchant_uid : 'merchant_' + new Date().getTime(),
-			name : data.name,
-			amount : data.price * document.getElementById('p-value').innerHTML ,
-			buyer_email : 'iamport@siot.do',
-			buyer_name : '구매자이름',
-			buyer_tel : '010-1234-5678',
-			buyer_addr : '서울특별시 강남구 삼성동',
-			buyer_postcode : '123-456',
-			kakaoOpenApp : true
+				pg : 'kakaopay',
+				pay_method : 'card',
+				merchant_uid : 'merchant_' + new Date().getTime(),
+				name : data.name,
+				amount : data.price * document.getElementById('p-value').innerHTML ,
+				buyer_email : userInfo.email,
+				buyer_name : userInfo.name,
+				buyer_tel : userInfo.phone,
+				buyer_addr : '서울특별시 강남구 삼성동',
+				buyer_postcode : '123-456',
+				kakaoOpenApp : true
 
-		}
-		, function(rsp) {
-			if ( rsp.success ) {
-				console.log(obj.userNo);
-				console.log(no.split("#")[0]);
-				$.ajax({ 
-					type: "POST",  
-					url: serverRoot + '/json/programMember/add', 
-					dataType: 'json',
-					data: {
-
-						uno: obj.userNo,
-						pno: no.split("#")[0]
-					}, 
-					success: function(result) { 
-						console.log(result);  
-					} 
-				})
-				.done(function(data) {
-
-
-					//[2] 서버에서 REST API로 결제정보확인 및 서비스루틴이 정상적인 경우
-					if ( everythings_fine ) {
-
-
-
-
-
-
-					} else {
-						//[3] 아직 제대로 결제가 되지 않았습니다.
-						//[4] 결제된 금액이 요청한 금액과 달라 결제를 자동취소처리하였습니다.
-					}
-				});
-			} else {
-				var msg = '결제에 실패하였습니다.';
-				msg += '에러내용 : ' + rsp.error_msg;
-
-				alert(msg);
 			}
-		});
-	}); 
-}
+			, function(rsp) {
+				if ( rsp.success ) {
+					console.log(obj.userNo);
+					console.log(no.split("#")[0]);
+					jQuery.ajax({ 
+						type: "POST",  
+						url: serverRoot + '/json/programMember/add', 
+						dataType: 'json',
+						data: {
+
+							uno: obj.userNo,
+							pno: no.split("#")[0]
+						}, 
+						success: function(result) { 
+							console.log(result);  
+						} 
+					})
+					.done(function(data) {
+
+
+						//[2] 서버에서 REST API로 결제정보확인 및 서비스루틴이 정상적인 경우
+						if ( everythings_fine ) {
+							var msg = '결제가 완료되었습니다.';
+							msg += '\n고유ID : ' + rsp.imp_uid;
+							msg += '\n상점 거래ID : ' + rsp.merchant_uid;
+							msg += '\결제 금액 : ' + rsp.paid_amount;
+							msg += '카드 승인번호 : ' + rsp.apply_num;
+
+							alert(msg);
+
+
+
+
+
+						} else {
+							//[3] 아직 제대로 결제가 되지 않았습니다.
+							//[4] 결제된 금액이 요청한 금액과 달라 결제를 자동취소처리하였습니다.
+						}
+					});
+				} else {
+					var msg = '결제에 실패하였습니다.';
+					msg += '에러내용 : ' + rsp.error_msg;
+
+					alert(msg);
+				}
+			});
+		}); 
+	}
 
 
 
