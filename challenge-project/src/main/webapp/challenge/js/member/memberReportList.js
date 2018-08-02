28a731cf752f2470c1413bf51adf1aac43137c67
soyoungkim9 <soyoung930623@gmail.com>
2018-08-02 21:42:06 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/member/memberReportList.js
+++ challenge-project/src/main/webapp/challenge/js/member/memberReportList.js
@@ -3,6 +3,7 @@ var pno;
 var defaultPage;
 var startDate;
 var endDate;
+var plno;
 
 // li-template 트레이너가 관리하는 프로그램 이름 목록
 var liTemplateSrc = $("#li-template").html();
@@ -105,7 +106,7 @@ window.onclick = function(event) {
     }
 }
 
-//원하는 회차의 운동계획서 확대 해서 보기!
+//원하는 회차의 운동계획서 확대 해서 보기! ********** 수정봐야됨 출석체크 여부
 $(document.body).on('click', '.editIcon', function(event) {
    event.preventDefault();
    dno = $(this).find('a').attr('data-dno');
@@ -130,8 +131,7 @@ $(document.body).on('click', '.editIcon', function(event) {
    });
 });
 
-
-//수정 버튼 눌렀을 경우 쿼리
+//수정 버튼 눌렀을 경우 쿼리 ********** 수정봐야됨 출석체크 여부
 $("#updatePlanButton").click(() => {
 	$.post(serverRoot + "/json/diary/update/", {
 		content: $("#modalViewContent textarea").val(),
@@ -158,4 +158,85 @@ $("#updatePlanButton").click(() => {
 	});
 });
 
+//새글 관련 이벤트
+$("#addPlan").click(() => {
+	pno = $('.active').find('a').attr('data-no');
+    // 프로그램 회차 관련
+    $.ajax(serverRoot + "/json/diary/list/" + pno + "/" + userInfo.userNo, {
+    	dataType: "json",
+	    success(data) {
+    		console.log(data);
+    	   $("option").remove();
+    	   var count = data.length + 1;
+    	   var turn;
+    	   if(data.length == 0) { // 프로그램에 운동일지 하나도 없을 경우
+    		   turn = 1;
+    	   } else { // 프로그램에 운동일지 한 개 이상 있을 경우
+    		   turn = data[0].program.proTurn;
+    	   }
+	       for (count; count <= turn; count++) {
+	     	  $('#mTurn').append("<option>" + count +"</option>");
+	       }
+	       
+	       var index = data.length;
+	       // plan에서 mDate와 mTitle을 불러 오기 위함!
+	       $.ajax(serverRoot + "/json/plan/list/" + pno, {
+	          dataType: "json",   
+	           success(data) {
+	   	       	  $("#mDate").val(data[index].planDate);
+		          $("#mTitle").val(data[index].planTitl);
+		          $("#mDate").attr("readonly", true);
+		          $("#mTitle").attr("readonly", true);
+	           },
+	           error() {
+	               window.alert("plan에서 mDate와 mTitle을 불러 오기 실행 오류!");
+	           }   
+	       });
+	       
+	       $("#mContent").val('');
+	       plno = data[index-1].no;
+	    },
+	    error() {
+	        window.alert("report.js addPlan 실행 오류!");
+	    }
+    });
+    
+    $('.modal').css("display", "block");
+    $('#viewForm').css("display", "none");
+    $('#addForm').css("display", "block");
+});
+
+//등록 버튼 눌렀을 시
+$("#registerPlan").click(() => {
+	var onOff;
+	if($("#checkType").val() == 'on') {
+		onOff = 1;
+	} else {
+		onOff = 0;
+	}
+	$.post(serverRoot + "/json/diary/add", {
+		dcheck: onOff,
+		content: $("#mContent").val(),
+		"no": plno,
+		"program.no": pno,
+		"uno": userInfo.userNo
+	}, () => {
+		modal.style.display = "none";
+		/* 업데이트한 상태에서 이전 화면으로 돌아가기 위한 코드임... 뭔가 이상 */
+		$.ajax(serverRoot + "/json/diary/list/" + pno + "/" + userInfo.userNo, {
+			dataType: "json",	
+		    success(data) {
+				 console.log("반영이 된것인가?" + pno);
+				 $('#programBox').html(programTemplateFn({
+					 name: data[0].program.name,
+					 startDate: data[0].program.startDate,
+					 endDate: data[0].program.endDate,
+					 list:data}));
+		    },
+		    error() {
+		        window.alert("report.js view list 실행 오류!");
+		    }	
+		});
+	});
+});
 
