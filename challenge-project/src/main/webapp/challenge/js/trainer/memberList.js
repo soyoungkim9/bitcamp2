6ac1ab5a721cef5586700b43126a7bd603724016
soyoungkim9 <soyoung930623@gmail.com>
2018-08-08 12:19:39 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/trainer/memberList.js
+++ challenge-project/src/main/webapp/challenge/js/trainer/memberList.js
@@ -104,12 +104,28 @@ $(document.body).on('click','.trSelect', function(event){
 				 name: data[0].program.name, 
 				 startDate: data[0].program.startDate,
 				 endDate: data[0].program.endDate}));
-			 $('#myModal').css("display", "block");
+			 	 console.log(data[0]);
+			 	 userNo = data[0].userNo;
 	    },
 	    error() {
-	        window.alert("report.js programTab AllList 실행 오류!");
+	        window.alert("meberList.js 회원정보 보기 관련 실행 오류!");
 	    }	
 	});
+	
+	// 회원 출석률 관련
+	$.ajax(serverRoot + "/json/diary/dList/" + userNo + "/" + programNum, {
+		dataType: "json",	
+	    success(data) {
+			console.log(data);
+			
+			
+			$('#myModal').css("display", "block");
+	    },
+	    error() {
+	        window.alert("meberList.js 출석률 관련 실행 오류!");
+	    }	
+	});
+	
 	$(document.body).on('click','.close', function(){
 		$('#myModal').css("display", "none");
 	})
@@ -167,89 +183,4 @@ $(document.body).on('click','.trSelect', function(event){
 			$('#myAddModal').css("display", "none");
 		});
 	});
-	
-
 });
-
-// 회원정보 보기
-//if (location.href.split("?").length > 1 && location.href.split("&").length > 1) {
-//	var pno = location.href.split("?")[1].split("&")[0].split("=")[1];
-//	var uno = location.href.split("&")[1].split("=")[1];
-//
-//	$.getJSON(serverRoot + "/json/programMember/" + pno + "/" + uno, function(data) {
-//		$("#mName").append(data[0].users.name);
-//        $("#mSex").append(data[0].users.sex);
-//        $("#mTel").append(data[0].users.userPhone);
-//        $("#mProgram").append(data[0].name);
-//				$("#mDate1").append(data[0].startDate);
-//				$("#mDate2").append(data[0].endDate);
-//        $("#mTarget").append(data[0].proGoal);
-//        $("#mAttend").append(data[0].diarys.dcheck);
-//        $("#mTargetPer").append(data[0].proGoalNum);
-//	});
-//	
-//	$('.modal').css("display", "block");
-//
-//	$(".close").click(function() {
-//		location.href=  "memberList.html";
-//		});
-//
-//} else if (location.href.split("?").length > 1) { // 프로그램별로 회원 리스트 보기
-//	var pno = location.href.split("?")[1].split("=")[1];
-//	$('#memberList tr').css('display','none');
-//	$.ajax(serverRoot + "/json/programMember/list/" + pno, {
-//		dataType: "json",	
-//	    success(data) {
-//	        for (var item of data) {
-//	        	var tr = document.createElement("tr");
-//	        	tr.innerHTML = "<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.users.email + "</a></td>" +
-//	        	"<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.users.name + "</a></td>" +
-//	        	"<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.users.sex + "</a></td>" +
-//	        	"<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.name +"</a></td>" +
-//	        	"<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.diarys.dcheck +"</a></td>";  
-//	        	memberList.appendChild(tr);
-//	        }
-//	    },
-//	    error() {
-//	        window.alert("member.js view list 실행 오류!");
-//	    }	
-//	});
-//} else if (serverRoot + "/challenge/html/trainer/memberList.html") { // 프로그램별로 회원 리스트 보기
-//	$.ajax(serverRoot + "/json/programMember/list", {
-//		dataType: "json",	
-//	    success(data) {
-//	        for (var item of data) {
-//	        	var tr = document.createElement("tr");
-//	        	tr.innerHTML = "<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.users.email + "</a></td>" +
-//	        	"<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.users.name + "</a></td>" +
-//	        	"<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.users.sex + "</a></td>" +
-//	        	"<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.name +"</a></td>" +
-//	        	"<td><a href='memberList.html?pno=" + item.no + 
-//	        	"&uno=" + item.users.userNo +"'>"
-//	        	+ item.diarys.dcheck +"</a></td>";  
-//	        	memberList.appendChild(tr);
-//	        }
-//	    },
-//	    error() {
-//	        window.alert("member.js view list 실행 오류!");
-//	    }	
-//	});
-//}
