4d4caf6c91828d754480d5b28be42613fc04977e
soyoungkim9 <soyoung930623@gmail.com>
2018-08-09 18:58:52 +0900

    .

--- challenge-project/src/main/webapp/challenge/js/search/search-sy.js
+++ challenge-project/src/main/webapp/challenge/js/search/search-sy.js
@@ -117,9 +117,23 @@ $(document.body).on('click', 'input:checkbox', function() {
 		data: {"pType": pType}
 	}).done(function(data) {
 		console.log(data);
-		for(var i = 0; i < da)
+		console.log(data.length);
+		for(var i = 0; i < data.length; i++) {
+			$('#aaa').html(cardBodyFn({list:data}));
+		}
 		
-		$('#aaa').html(cardBodyFn({list:data}));
+		  var i;
+		  for (i = 0; i < data.length; i++) {
+		    dday(data[i].startDate, i); //D-day
+		    reviewScore(data[i].no, i); //별점,리뷰 개수
+		    trImg(data[i].trainerNo, i);
+		    pmemberCount(data[i].no, i);
+		    var price = addComma($(".numberic-"+i+"").html())
+		    var place = ($(".card-body-local-"+i+"").html()).substring(3, 6);
+		    $(".numberic-"+i+"").html(price)
+		    $(".card-body-local-"+i+"").html(place)
+		  }
+		
 	});
 
 });
