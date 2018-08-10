56e7b91a70eadedaba0484b00a9e9050c99f7238
soyoungkim9 <soyoung930623@gmail.com>
2018-08-10 10:21:28 +0900

    .

--- challenge-project/src/main/java/challenge/dao/ProgramMemberDao.java
+++ challenge-project/src/main/java/challenge/dao/ProgramMemberDao.java
@@ -1,6 +1,7 @@
 package challenge.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import challenge.domain.Member;
 import challenge.domain.ProgramMember;
@@ -10,6 +11,7 @@ public interface ProgramMemberDao {
     List<ProgramMember> pNameList(int trnNo); // 프로그램 이름 리스트
     List<ProgramMember> lectList(int uno); // 회원이 수강중인 프로그램 이름 리스트
     List<ProgramMember> selectList(int trnNo); // 전체 리스트
+    List<ProgramMember> searchList(Map<String,Object> params); // 회원명 검색
     List<ProgramMember> selectListWithProgram(int pno, int trnNo); // 프로그램에 속한 회원 목록 뽑기
     List<ProgramMember> selectOne(int pno, int userNo); // 한명만 선택해서 보기
     Member selectListWithUno(int userNo); // 유저가 듣는 프로그램 모든 정보 받아오기
--- challenge-project/src/main/java/challenge/service/ProgramMemberService.java
+++ challenge-project/src/main/java/challenge/service/ProgramMemberService.java
@@ -10,6 +10,7 @@ public interface ProgramMemberService {
     List<ProgramMember> listWithPname(int trnNo);
     List<ProgramMember> listWithLect(int uno);
     List<ProgramMember> list(int trnNo); // 전체 프로그램에 참여중인 회원 목록
+    List<ProgramMember> listWithSearch(int pno, int trnNo, String uName); // 전체 프로그램에 참여중인 회원 목록
     List<ProgramMember> list(int pno, int trnNo); // 해당 프로그램에 참여중인 회원 목록
     List<ProgramMember> get(int pno, int userNo); // 한 회원의 정보 상세보기
     Member getWithUserNo(int userNo);
