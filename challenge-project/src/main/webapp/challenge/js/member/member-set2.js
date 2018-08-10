3af4c76d13119326221f47193b98c9e8e67a8fd2
hong <hong@DESKTOP-1JD9VQR>
2018-08-10 11:21:57 +0900

    aa

--- challenge-project/src/main/webapp/challenge/js/member/member-set2.js
+++ challenge-project/src/main/webapp/challenge/js/member/member-set2.js
@@ -1,3 +1,16 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+
 $.getJSON(serverRoot + "/json/bodyInfo/" + userInfo.userNo, (data) => {
 	console.log(data[data.length-1]);
     var lastData = data[data.length-1];
