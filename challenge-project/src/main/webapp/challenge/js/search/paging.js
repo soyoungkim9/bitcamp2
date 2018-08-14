7388a75f09ed1c691eddb4648159985e447e0d03
hong <hong@DESKTOP-1JD9VQR>
2018-08-14 12:22:44 +0900

    1

--- challenge-project/src/main/webapp/challenge/js/search/paging.js
+++ challenge-project/src/main/webapp/challenge/js/search/paging.js
@@ -1,4 +1,6 @@
-var = pageCount
+var pageCount = 1 ;
+
+
 
 $('#priceSearch').click(() => {
 	$.ajax({
@@ -6,18 +8,69 @@ $('#priceSearch').click(() => {
 		type: 'POST',
 		async: false,
 		traditional : true,
-		url: serverRoot + '/json/program/pList/'+pageCount+'/12',
+		url: serverRoot + '/json/program/pList',
 		data: {
 			minPrice : $('#from_id').val(),
 			maxPrice : $('#to_id').val()
 		}
 	}).done(function(data){
-
-		for(var i = 0; i < data.length; i++) {
-			$('#aaa').html(cardBodyFn({list:data}));
-		}
-		loadCards(data);
-	})
+		var totalPage = data.length;
+		var onePage = Math.ceil(totalPage / 9) ;
+		console.log(totalPage);
+		/*console.log(onePage);*/
+		$.ajax({
+			dataType : 'json',
+			type: 'POST',
+			async: false,
+			traditional : true,
+			url: serverRoot + '/json/program/pList/1/9',
+			data: {
+				minPrice : $('#from_id').val(),
+				maxPrice : $('#to_id').val()
+			}
+		}).done(function(data){
+			$("#paging").empty();
+			
+			for(var i = 0; i < data.length; i++) {
+				$('#aaa').html(cardBodyFn({list:data}));
+			}
+			
+			for(pageCount = 1; pageCount < onePage+1; pageCount++){
+				$('#paging').append('<a data-no="'+pageCount+'" class="sm-pagination-button" href="#">'+pageCount+'</a>')
+				
+			}
+			
+			loadCards(data);
+			
+		})
+		
+		$('.sm-pagination-button').click( function(){
+		    if ( $(this).hasClass('pageActive') ) {
+		        $(this).removeClass('pageActive');
+		    } else {
+		        $('.sm-pagination-button').removeClass('pageActive');
+		        $(this).addClass('pageActive');    
+		    }
+		    var pageNum = $('.pageActive').attr("data-no");
+		    $.ajax({
+				dataType : 'json',
+				type: 'POST',
+				async: false,
+				traditional : true,
+				url: serverRoot + '/json/program/pList/'+pageNum+'/9',
+				data: {
+					minPrice : $('#from_id').val(),
+					maxPrice : $('#to_id').val()
+				}
+			}).done(function(data){
+				for(var i = 0; i < data.length; i++) {
+				$('#aaa').html(cardBodyFn({list:data}));
+			}
+				loadCards(data);
+			})
+		});
+		
+	})  // pList done
 
 });
 
