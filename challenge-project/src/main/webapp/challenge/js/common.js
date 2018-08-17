f0013edced7cfdc7ded7490c0672e4547f32c177
pparksuuu <supr2000@gmail.com>
2018-08-17 20:58:56 +0900

    BackSpace 뒤로가기 이벤트 추가

--- challenge-project/src/main/webapp/challenge/js/common.js
+++ challenge-project/src/main/webapp/challenge/js/common.js
@@ -1,1 +1,13 @@
 var serverRoot = "http://localhost:8888/challenge-project";
+
+document.addEventListener("keydown", KeyCheck);
+
+function KeyCheck(event)
+{
+   var KeyID = event.keyCode;
+   if (KeyID == 8) {
+	   window.history.back();
+   }
+}
+
+
